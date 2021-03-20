c24a7856d22e979a3d15e29787c515bffe3c2720
Michael Naghavipour <MichaelNaghavipour@users.noreply.github.com>
2021-03-21 01:38:24 +0330

    Create Solution2.js

--- Solutions/Solution2.js
+++ Solutions/Solution2.js
@@ -1,21 +1,23 @@
-function productExceptSelf(nums) {
-  if (nums.length === 0) {
+const inputArray = [1, 2, 3, 4, 5]
+
+function productOfArray(inputArray) {
+  if (inputArray.length == 0) {
     return [];
   }
 
-  const productArr = Array(nums.length);
-  productArr[0] = 1;
-  for (let i = 1; i < productArr.length; i++) {
-    // get product of previous numbers
-    productArr[i] = productArr[i - 1] * nums[i - 1];
+  const outputArray = Array(inputArray.length);
+  outputArray[0] = 1;
+  for (let i = 1; i < outputArray.length; i++) {
+    outputArray[i] = outputArray[i - 1] * inputArray[i - 1];
   }
 
-  // get product numbers right of self
-  let product = 1;
-  for (let i = productArr.length - 1; i >= 0; i--) {
-    productArr[i] *= product;
-    product *= nums[i];
+  let rightSideProduct = 1;
+  for (let i = outputArray.length - 1; i >= 0; i--) {
+    outputArray[i] *= rightSideProduct;
+    rightSideProduct *= inputArray[i];
   }
 
-  return productArr;
+  return outputArray;
 }
+
+console.log(productOfArray(inputArray))
